9ce6ab634f6c04bf650de90f8627a714f809f9f2
tatv-dev <trinhvanta1804@gmail.com>
2024-12-20 10:45:17 +0700

    over logo

--- ios/Classes/G3StreamView.m
+++ ios/Classes/G3StreamView.m
@@ -11,7 +11,7 @@
         self.imageView = [[UIImageView alloc] initWithFrame:frame];
         self.imageView.contentMode = UIViewContentModeScaleAspectFit;
         _view = self.imageView;
-        //[self initView];
+        [self initView];
         self.processingQueue = dispatch_queue_create("frameProcessingQueue",  DISPATCH_QUEUE_SERIAL);
         self.frameQueue = [NSMutableArray array];
         [self startSocket];
@@ -21,48 +21,45 @@
 }
 
 -(void)initView{
-    //init image view
-    self.imageView = [[UIImageView alloc] initWithFrame:_view.bounds];
-    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    [_view addSubview:self.imageView];
-    
-//    // LinearLayout equivalent
-//    UIView *linearLayout = [[UIView alloc] init];
-//    linearLayout.translatesAutoresizingMaskIntoConstraints = NO;
-//    linearLayout.backgroundColor = [UIColor clearColor];
-//    [_view addSubview:linearLayout];
-//
-//    // Set layout constraints for linearLayout
-//    [NSLayoutConstraint activateConstraints:@[
-//        [linearLayout.trailingAnchor constraintEqualToAnchor:_view.trailingAnchor],
-//        [linearLayout.bottomAnchor constraintEqualToAnchor:_view.bottomAnchor],
-//        [linearLayout.widthAnchor constraintEqualToAnchor:_view.widthAnchor],
-//        [linearLayout.heightAnchor constraintEqualToAnchor:_view.heightAnchor]
-//    ]];
-//
-//    // Add ImageView (logo)
-////    UIImageView *logoImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo_white"]];
-////    logoImageView.translatesAutoresizingMaskIntoConstraints = NO;
-////    [linearLayout addSubview:logoImageView];
-////
-////    // Add layout constraints for logoImageView
-////    [NSLayoutConstraint activateConstraints:@[
-////        [logoImageView.trailingAnchor constraintEqualToAnchor:linearLayout.trailingAnchor constant:-8],
-////        [logoImageView.bottomAnchor constraintEqualToAnchor:linearLayout.bottomAnchor]
-////    ]];
-//
-//    // Add TextView (meta_data equivalent)
-//    self.metaDataLabel = [[UILabel alloc] init];
-//    self.metaDataLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.metaDataLabel.font = [UIFont systemFontOfSize:8];
-//    self.metaDataLabel.textColor = [UIColor whiteColor];
-//    [linearLayout addSubview:self.metaDataLabel];
-//
-//    // Add layout constraints for metaDataLabel
-//    [NSLayoutConstraint activateConstraints:@[
-//        [self.metaDataLabel.trailingAnchor constraintEqualToAnchor:linearLayout.trailingAnchor constant:-20],
-//        [self.metaDataLabel.bottomAnchor constraintEqualToAnchor:linearLayout.bottomAnchor]
-//    ]];
+    // LinearLayout equivalent
+    UIView *linearLayout = [[UIView alloc] init];
+    linearLayout.translatesAutoresizingMaskIntoConstraints = NO;
+    linearLayout.backgroundColor = [UIColor clearColor];
+    [_view addSubview:linearLayout];
+
+    // Set layout constraints for linearLayout
+    [NSLayoutConstraint activateConstraints:@[
+        [linearLayout.trailingAnchor constraintEqualToAnchor:_view.trailingAnchor],
+        [linearLayout.bottomAnchor constraintEqualToAnchor:_view.bottomAnchor],
+        [linearLayout.widthAnchor constraintEqualToAnchor:_view.widthAnchor],
+        [linearLayout.heightAnchor constraintEqualToAnchor:_view.heightAnchor]
+    ]];
+
+     //Add ImageView (logo)
+    UIImage* image = [UIImage imageNamed:@"logo_white.png"];
+    UIImageView *logoImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo_white.png"]];
+    logoImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    [linearLayout addSubview:logoImageView];
+
+    // Add layout constraints for logoImageView
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    [NSLayoutConstraint activateConstraints:@[
+        [logoImageView.trailingAnchor constraintEqualToAnchor:linearLayout.trailingAnchor constant:-screenWidth*0.37],
+        [logoImageView.bottomAnchor constraintEqualToAnchor:linearLayout.bottomAnchor constant:-3]
+    ]];
+
+    // Add TextView (meta_data equivalent)
+    self.metaDataLabel = [[UILabel alloc] init];
+    self.metaDataLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.metaDataLabel.font = [UIFont systemFontOfSize:8];
+    self.metaDataLabel.textColor = [UIColor whiteColor];
+    [linearLayout addSubview:self.metaDataLabel];
+
+    // Add layout constraints for metaDataLabel
+    [NSLayoutConstraint activateConstraints:@[
+        [self.metaDataLabel.trailingAnchor constraintEqualToAnchor:linearLayout.trailingAnchor constant:-20],
+        [self.metaDataLabel.bottomAnchor constraintEqualToAnchor:linearLayout.bottomAnchor constant:-3]
+    ]];
 }
 
 -(void) displayFrame {
@@ -109,8 +106,8 @@
         }
         if (message.type == NSURLSessionWebSocketMessageTypeData) {
             [self.frameQueue addObject:message.data];
-        } else {
-            self.metaData = message.data;
+        } else if (message.type == NSURLSessionWebSocketMessageTypeString) {
+            self.metaData = message.string;
         }
         
         if (strongSelf.isStreaming) {
@@ -122,7 +119,6 @@
 }
 
 -(void) processFrameData:(NSData*) data {
-    NSLog(@"Vo day r ma sao khong hien%d", data.length);
     UIImage *image = [UIImage imageWithData:[self decompressData:data]];
     if(image) {
         dispatch_async(dispatch_get_main_queue(), ^{
