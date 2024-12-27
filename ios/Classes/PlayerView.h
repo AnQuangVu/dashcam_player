62434bf75666e46811fccbf97d415a1c1c3459ec
DESKTOP-SRO79J6\ADMIN <ngocanhdt.icv@gmail.com>
2024-12-27 13:58:03 +0700

    no message

--- ios/Classes/PlayerView.h
+++ ios/Classes/PlayerView.h
@@ -4,6 +4,11 @@
 
 @interface PlayerView : NSObject <FlutterPlatformView>
 @property(nonatomic, strong) VLCMediaPlayer *mediaPlayer;
+@property (nonatomic, strong) UIButton *playPauseButton;
+@property (nonatomic, strong) UISlider *progressSlider;
+@property (nonatomic, strong) UILabel *currentTimeLabel;
+@property (nonatomic, strong) UILabel *durationLabel;
+@property (nonatomic, strong) NSTimer *progressTimer;
 
 - (instancetype _Nullable )initWithFrame:(CGRect)frame
                viewIdentifier:(int64_t)viewId
